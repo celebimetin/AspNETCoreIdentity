6b7c0c2da1bfcf579b4752965092855a7c2b3dd2
Metin Çelebi <metincelebi5534@gmail.com>
2022-07-09 13:17:54 +0300

    Üye ol(SignUp) sayfasının tasarımı ve kodlanması tamamlandı B.2 - D.11

--- IdentityWebApp/Controllers/HomeController.cs
+++ IdentityWebApp/Controllers/HomeController.cs
@@ -1,12 +1,65 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityWebApp.Models;
+using IdentityWebApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace IdentityWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private UserManager<AppUser> _userManager { get; }
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserViewModel userViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser();
+                user.UserName = userViewModel.UserName;
+                user.Email = userViewModel.Email;
+                user.PhoneNumber = userViewModel.PhoneNumber;
+                IdentityResult result = await _userManager.CreateAsync(user, userViewModel.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (IdentityError item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(userViewModel);
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Login()
+        //{
+        //    return View();
+        //}
     }
 }
---
+++ IdentityWebApp/ViewModels/UserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityWebApp.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required(ErrorMessage = "Kullanıcı ismi gereklidir.")]
+        [Display(Name = "Kullanıcı Adı")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Telefon Numarası")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Email Adresi gereklidir.")]
+        [Display(Name = "Email Adresi")]
+        [EmailAddress(ErrorMessage = "Email adresi doğru formatta değildir.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Şifre gereklidir.")]
+        [Display(Name = "Şifre")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
