a8fc31358135faa65f8312267b27a6632f5371ed
Metin Çelebi <metincelebi5534@gmail.com>
2022-08-11 19:01:23 +0300

    Admin panelde kullanıcı şifre değiştirme tamamlandı.

---
+++ IdentityWebApp/ViewModels/PasswordResetByAdminViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityWebApp.ViewModels
+{
+    public class PasswordResetByAdminViewModel
+    {
+        public string UserId { get; set; }
+        [Required(ErrorMessage = "Zorunlu alan")]
+        [Display(Name = "Yeni Şifre")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+    }
+}
